0ae80adc450a248ecb700095ac783b83fedc86cf
Tak <takeshiseodev@gmail.com>
2024-01-30 16:05:15 -0500

    Scaffold Components with Placeholder Content

--- src/components/Accounts.jsx
+++ src/components/Accounts.jsx
@@ -1,9 +1,22 @@
 /** @format */
 import React from 'react'
 
-function Accounts() {
+function Accounts({ data }) {
     return (
-        <h1>Accounts</h1>
+        <section>
+            <h2>Accounts</h2>
+            <div>
+                {data.map((account) => {
+                    const { id, title, balance } = account
+                    return (
+                        <article key={id}>
+                            <h3>{title}</h3>
+                            <p>${balance}</p>
+                        </article>
+                    )
+                })}
+            </div>
+        </section>
     )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,23 @@ import React from 'react'
 function NavBar() {
     return (
         <nav>
-            <h1>NavBar</h1>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Payments</a>
+                </li>
+                <li>
+                    <a href="#">Savings</a>
+                </li>
+                <li>
+                    <a href="#">Financing</a>
+                </li>
+                <li>
+                    <a href="#">Stocks</a>
+                </li>
+            </ul>
         </nav>
     )
 }
